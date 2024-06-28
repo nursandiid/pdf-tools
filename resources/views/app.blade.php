2cc4b01b495d7bcabccc62782206fdd5a657d637
Nursandi <sandinur157@gmail.com>
2024-06-28 09:38:50 +0700

    feat: Set up Prettier and TailwindCSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,13 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Icon -->
+    <link rel="icon" href="{{ asset('img/favicon.png') }}">
+
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,45 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.tsx",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
+        blue: {
+          DEFAULT: colors.blue[600],
+          dark: colors.blue[700],
+        },
+        green: {
+          DEFAULT: colors.emerald[600],
+          dark: colors.emerald[700],
+        },
+        primary: {
+          DEFAULT: colors.red[600],
+          dark: colors.red[700],
+        },
+      },
     },
+  },
 
-    plugins: [forms],
+  darkMode: ["class"],
+
+  plugins: [forms],
 };
