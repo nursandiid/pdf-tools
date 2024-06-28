0491270fe6892b104c2611aa273b346c2195434c
Nursandi <sandinur157@gmail.com>
2024-06-28 09:23:04 +0700

    feat: Set up a fresh Laravel app and Install Breeze

---
+++ resources/js/app.tsx
@@ -0,0 +1,28 @@
+import "./bootstrap";
+import "../css/app.css";
+
+import { createRoot, hydrateRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob("./pages/**/*.tsx")
+        ),
+    setup({ el, App, props }) {
+        if (import.meta.env.DEV) {
+            createRoot(el).render(<App {...props} />);
+            return;
+        }
+
+        hydrateRoot(el, <App {...props} />);
+    },
+    progress: {
+        color: "#4B5563",
+    },
+});
